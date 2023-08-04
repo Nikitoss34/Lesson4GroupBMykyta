c7d06e4dec3db9d91bc3092d4fc374359a4e64e2
Mykyta <honcharov.nikita70@gmail.com>
2023-08-04 19:32:15 +0200

    second message

--- Src/ProgramLesson04.java
+++ Src/ProgramLesson04.java
@@ -1,5 +1,6 @@
 public class ProgramLesson04 {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("hello git from Mykyta");
     }
 }
